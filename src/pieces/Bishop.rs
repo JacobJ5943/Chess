83cbf486d9924c6662868850ab547ff9e6523e7b
Jacob Johnson <Johnson.Jacob5943@gmail.com>
2021-05-13 12:57:15 -0500

    Move Bishop's diagonal check to function.

--- src/pieces/Bishop.rs
+++ src/pieces/Bishop.rs
@@ -1,4 +1,5 @@
 use super::super::piece_types::{PieceColor, QuickPiece};
+use super::{coord_on_board, check_if_piece_on_location};
 use super::PieceMove;
 
 pub struct Bishop {
@@ -7,8 +8,8 @@ pub struct Bishop {
     piece_color: PieceColor,
 }
 
-
 impl Bishop {
+
     pub fn new(pos_x: usize, pos_y: usize, piece_color: PieceColor) -> Bishop {
         Bishop {
             pos_x,
@@ -16,15 +17,17 @@ impl Bishop {
             piece_color,
         }
     }
-}
 
-impl Bishop {
-    fn check_if_piece_on_location(&self, x_coord: usize, y_coord: usize, quick_board: &Vec<Vec<QuickPiece>>) -> bool{
-        let quick_piece_row = quick_board.get(x_coord);
-        true
+    fn is_move_diagonal(&self, x_coord: usize, y_coord: usize, quick_board: &Vec<Vec<QuickPiece>>) -> bool{
+        let x_delta = usize::max(x_coord, self.pos_x) - usize::min(x_coord, self.pos_x);
+        let y_delta = usize::max(y_coord, self.pos_y) - usize::min(y_coord, self.pos_y);
+        // This is to cover the case where the piece isn't moving anywhere
+        if x_delta == 0 {
+            return false
+        }
+
+        x_delta !=0 && x_delta==y_delta
     }
-
-
     /// This function should return  true if there are any pieces in the path to the final location.
     /// This does NOT include the final location
     /// This function assumes that the path it's going to is a valid location for a bishop
@@ -57,30 +60,21 @@ impl Bishop {
 
         false
     }
+
 }
 
 impl PieceMove for Bishop {
 
     // @TODO All the returns feel wrong
     fn can_move(&self, x_coord: usize, y_coord: usize, quick_board: &Vec<Vec<QuickPiece>>) -> bool {
-        let y_length = match quick_board.get(0) {
-            Some(row) => row.len(),
-            None => return false // @TODO Look into if this is rusty or crusty
-        };
-
-        if x_coord >= quick_board.len() || y_coord >= y_length {
-             return false
-        }
-        let x_delta = usize::max(x_coord, self.pos_x) - usize::min(x_coord, self.pos_x);
-        let y_delta = usize::max(y_coord, self.pos_y) - usize::min(y_coord, self.pos_y);
-        // This is to cover the case where the piece isn't moving anywhere
-        if x_delta == 0 {
+        if !coord_on_board(x_coord, y_coord, quick_board) {
             return false
         }
 
-        if x_delta != y_delta {
-            return false
+        if !self.is_move_diagonal(x_coord,y_coord,quick_board) {
+            return false;
         }
+
 
         // @TODO This may not need to be this functions responsibility anymore
         // Check if there are pieces in the way
@@ -90,8 +84,9 @@ impl PieceMove for Bishop {
             return false
         }
 
-        let mut piece_on_location_result = false;
-        if self.check_if_piece_on_location(x_coord,y_coord,quick_board) {
+        let mut piece_on_location_result = true;
+        // @TODO this check could be refactored out so there is less doubling of work
+        if check_if_piece_on_location(x_coord,y_coord, quick_board) {
             let piece = quick_board.get(x_coord).unwrap().get(y_coord).unwrap();
             piece_on_location_result = match piece {
                 QuickPiece::PIECE(color) => !(*color == self.piece_color),
--- src/pieces/mod.rs
+++ src/pieces/mod.rs
@@ -6,6 +6,8 @@ pub mod queen;
 pub mod rook;
 
 use super::piece_types;
+use crate::piece_types::QuickPiece;
+
 // Calling it PieceMove because I don't want to bother with if Move is already a trait or something
 // silly
 pub trait PieceMove {
@@ -16,4 +18,26 @@ pub trait PieceMove {
         quick_board: &Vec<Vec<piece_types::QuickPiece>>,
     ) -> bool;
     fn moves_on_board(&self) -> Vec<(usize, usize)>;
+}
+
+pub fn coord_on_board(x_coord: usize, y_coord: usize, quick_board: &Vec<Vec<piece_types::QuickPiece>>) -> bool {
+    let y_length = match quick_board.get(0) {
+        Some(row) => row.len(),
+        None => return false // @TODO Look into if this is rusty or crusty
+    };
+
+    if x_coord >= quick_board.len() || y_coord >= y_length {
+        return false
+    }
+    true
+}
+
+fn check_if_piece_on_location(x_coord:usize, y_coord:usize, quick_board:&Vec<Vec<piece_types::QuickPiece>>) -> bool {
+    let piece = quick_board.get(x_coord).unwrap().get(y_coord).unwrap();
+    match piece {
+        piece_types::QuickPiece::PIECE(color) => true,
+        piece_types::QuickPiece::KING(color) => true,
+        _ => false
+    }
+
 }
